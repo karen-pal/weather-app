4e8868c1f6d57c4c0fc92a1b9d49e65458751bfc
Milagros Gonzalez <mmilagrosgon@gmail.com>
2019-05-31 14:32:35 -0300

    Se cambió la forma de pasarle datos a Current y renderizarlo.

--- src/components/Current.jsx
+++ src/components/Current.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import axios from "axios";
 
+function zeroPad(n) {
+  return n < 10 ? '0' + n : String(n);
+}
+
+function capitalizar(string) {
+  return string[0].toUpperCase() + string.substring(1);
+}
+
 class Current extends React.Component {
   constructor(props) {
     super(props);
@@ -10,17 +18,24 @@ class Current extends React.Component {
     if (this.props.info !== undefined) {
       const info = this.props.info;
       // Esto borra todos los espacios blancos de cityCode.
-      const cityCode = info.cityCode.replace(/\s+/g, '');
-      const city = cityCode.replace(/,.*$/, '');
-      const country = cityCode.replace(/^.*,/, '').toUpperCase();
+      console.log(info.data);
+      const sunset = new Date(info.data.sys.sunset * 1000);
+      const sunrise = new Date(info.data.sys.sunrise * 1000);
 
       return (
         <div className='current'>
-          <h1>{city} ({country})</h1>
+          <h1>{info.city} ({info.country})</h1>
           <div>
-            Temperatura: {(info.weather.temp - 273.15).toFixed(2)} °C <br />
-            Presión: {info.weather.pressure / 1000} mbar <br />
-            Humedad: {info.weather.humidity} %
+            Temperatura: {info.data.main.temp} °C <br />
+            {capitalizar(info.data.weather[0].description)} <br />
+            Presión: {info.data.main.pressure / 1000} mbar <br />
+            Humedad: {info.data.main.humidity} % <br />
+            Temperatura Mínima: {info.data.main.temp_min} °C <br />
+            Temperatura Máxima: {info.data.main.temp_max} °C <br />
+            Salida del sol: {zeroPad(sunrise.getHours())}:{zeroPad(sunrise.getMinutes())} hs <br />
+            Puesta del sol: {zeroPad(sunset.getHours())}:{zeroPad(sunset.getMinutes())} hs <br />
+            Viento: {info.data.wind.speed} m/s <br />
+
           </div>
         </div>
       );
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 class Weather extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { loading: false, info: undefined };
+    this.state = { loading_current: false, loading_forecast: false };
 
     this.lastCityCode = undefined;
     this.appId = "450a05a5d3129eb605e57f58998e484d";
@@ -23,36 +23,49 @@ class Weather extends React.Component {
   updateCity() {
     if (this.lastCityCode !== this.props.cityCode) {
       this.lastCityCode = this.props.cityCode;
-      this.setState({ loading: true });
+      this.setState({ loading_current: true, loading_forecast: true });
 
-      const cityCode = this.props.cityCode.replace(/\s+/g, ''); // TEMPORAL.
+      const cityCode = this.props.cityCode.replace(/\s+/g, '');
+      const city = cityCode.replace(/,.*$/, '');
+      const country = cityCode.replace(/^.*,/, '').toUpperCase();
 
-
-
-      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityCode}&appid=${this.appId}`)
+      // Petición para Current
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${this.appId}&units=metric&lang=es`)
       .then(response => {
         this.setState({
-          info: {
-            cityCode,
-            weather: response.data.main
-          }, loading: false });
+          info_current: { city, country, data: response.data },
+          loading_current: false
+        });
       })
       .catch(err => {
         console.assert(err.message === "Request failed with status code 404");
         console.log(err);
       });
+
+      // Petición para Forecast
+      axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&appid=${this.appId}&units=metric&lang=es`)
+      .then(response => {
+        //console.log(response.data)
+        this.setState({
+          info_forecast: { city, country, data: response.data },
+          loading_forecast: false
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      })
     }
   }
 
   render() {
-    if (this.state.loading) {
+    if (this.state.loading_current || this.state.loading_forecast) {
       return <div className='weather'>Cargando...</div>;
     }
 
     return (
       <div className='weather'>
-        <Current info={this.state.info} />
-        <Forecast info={this.state.info} />
+        <Current info={this.state.info_current} />
+        <Forecast info={this.state.info_forecast} />
       </div>
     );
   }
